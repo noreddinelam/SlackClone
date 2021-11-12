b8635a241f0511113e45cd78e89f373bd8c8fefa
Nouredine <in_lamamra@esi.dz>
2021-11-12 23:13:58 +0100

    configure pom.xml file and add testing librairies

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void testExample(){
+        assertEquals(5,5);
+    }
+
+}
