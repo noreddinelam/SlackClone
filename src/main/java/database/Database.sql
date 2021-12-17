f2f023411aa04405c06c6d6a9b296b5ea640065e
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2021-12-17 14:32:33 +0100

    [Database] create the init file

--- src/main/java/database/Database.sql
+++ src/main/java/database/Database.sql
@@ -1,6 +1,16 @@
 -- ****************** SqlDBM: MySQL ******************;
 -- ***************************************************;
 
+use `slack-clone-db`;
+
+-- ************************************** `Client`
+CREATE TABLE `Client`
+(
+    `username` integer NOT NULL ,
+    `password` varchar(45) NOT NULL ,
+
+    PRIMARY KEY (`username`)
+);
 
 -- ************************************** `Channel`
 
@@ -16,14 +26,23 @@ PRIMARY KEY (`id`),
 KEY `FK_26` (`idAdmin`),
 CONSTRAINT `FK_24` FOREIGN KEY `FK_26` (`idAdmin`) REFERENCES `Client` (`username`)
 );
--- ************************************** `Client`
-CREATE TABLE `Client`
-(
- `username` integer NOT NULL ,
- `password` varchar(45) NOT NULL ,
 
-PRIMARY KEY (`username`)
+-- ************************************** `Message`
+CREATE TABLE `Message`
+(
+    `id`        integer NOT NULL ,
+    `content`   varchar(255) NOT NULL ,
+    `idChannel` integer NOT NULL ,
+    `idUser`    integer NOT NULL ,
+    `date`      datetime NOT NULL ,
+
+    PRIMARY KEY (`id`),
+    KEY `FK_19` (`idUser`),
+    CONSTRAINT `FK_17` FOREIGN KEY `FK_19` (`idUser`) REFERENCES `Client` (`username`),
+    KEY `FK_22` (`idChannel`),
+    CONSTRAINT `FK_20` FOREIGN KEY `FK_22` (`idChannel`) REFERENCES `Channel` (`id`)
 );
+
 -- ************************************** `Client_Channel`
 CREATE TABLE `Client_Channel`
 (
@@ -35,21 +54,7 @@ CONSTRAINT `FK_28` FOREIGN KEY `FK_30` (`idChannel`) REFERENCES `Channel` (`id`)
 KEY `FK_33` (`username`),
 CONSTRAINT `FK_31` FOREIGN KEY `FK_33` (`username`) REFERENCES `Client` (`username`)
 );
--- ************************************** `Message`
-CREATE TABLE `Message`
-(
- `id`        integer NOT NULL ,
- `content`   varchar(255) NOT NULL ,
- `idChannel` integer NOT NULL ,
- `idUser`    integer NOT NULL ,
- `date`      datetime NOT NULL ,
 
-PRIMARY KEY (`id`),
-KEY `FK_19` (`idUser`),
-CONSTRAINT `FK_17` FOREIGN KEY `FK_19` (`idUser`) REFERENCES `Client` (`username`),
-KEY `FK_22` (`idChannel`),
-CONSTRAINT `FK_20` FOREIGN KEY `FK_22` (`idChannel`) REFERENCES `Channel` (`id`)
-);
 
 
 
