bcfd569ca0a92345349ae64702ba9ca0b8692324
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2021-12-21 22:46:11 +0100

    [Database] change database schema

--- src/main/java/database/Database.sql
+++ src/main/java/database/Database.sql
@@ -29,30 +29,33 @@ CONSTRAINT `FK_24` FOREIGN KEY `FK_26` (`idAdmin`) REFERENCES `Client` (`usernam
 
 CREATE TABLE `Message`
 (
- `id`        integer NOT NULL ,
+ `id`        integer NOT NULL AUTO_INCREMENT,
  `content`   varchar(255) NOT NULL ,
  `idChannel` varchar(45) NOT NULL ,
+ `username` varchar(45) NOT NULL,
  `date`      datetime NOT NULL ,
 
 PRIMARY KEY (`id`),
 KEY `FK_22` (`idChannel`),
-CONSTRAINT `FK_20` FOREIGN KEY `FK_22` (`idChannel`) REFERENCES `Channel` (`name`)
-);
-
--- ************************************** `Client_Channel`
-CREATE TABLE `Client_Channel_Message`
-(
- `idChannel` varchar(45) NOT NULL ,
- `username`  varchar(45) NOT NULL ,
- `idMessage` integer NOT NULL,
-
-KEY `FK_30` (`idChannel`),
-CONSTRAINT `FK_28` FOREIGN KEY `FK_30` (`idChannel`) REFERENCES `Channel` (`name`),
+CONSTRAINT `FK_20` FOREIGN KEY `FK_22` (`idChannel`) REFERENCES `Channel` (`name`),
 KEY `FK_33` (`username`),
-CONSTRAINT `FK_31` FOREIGN KEY `FK_33` (`username`) REFERENCES `Client` (`username`),
-KEY `FK_36` (`idMessage`),
-CONSTRAINT  `FK_34` FOREIGN KEY `FK_36` (`idMessage`) REFERENCES `Message` (`id`)
+CONSTRAINT `FK_31` FOREIGN KEY `FK_33` (`username`) REFERENCES `Client` (`username`)
 );
+
+# -- ************************************** `Client_Channel`
+# CREATE TABLE `Client_Channel_Message`
+# (
+#  `idChannel` varchar(45) NOT NULL ,
+#  `username`  varchar(45) NOT NULL ,
+#  `idMessage` integer NOT NULL,
+#
+# KEY `FK_30` (`idChannel`),
+# CONSTRAINT `FK_28` FOREIGN KEY `FK_30` (`idChannel`) REFERENCES `Channel` (`name`),
+# KEY `FK_33` (`username`),
+# CONSTRAINT `FK_31` FOREIGN KEY `FK_33` (`username`) REFERENCES `Client` (`username`),
+# KEY `FK_36` (`idMessage`),
+# CONSTRAINT  `FK_34` FOREIGN KEY `FK_36` (`idMessage`) REFERENCES `Message` (`id`)
+# );
 
 
 
