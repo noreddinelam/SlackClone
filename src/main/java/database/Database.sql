0e0405e290321779229cc1f7376ff6eb4f831678
yecagp <54538746+yecagp@users.noreply.github.com>
2021-12-25 23:20:25 +0100

    Update Database.sql

--- src/main/java/database/Database.sql
+++ src/main/java/database/Database.sql
@@ -18,7 +18,7 @@ CREATE TABLE `Channel`
  `channelName`     varchar(45) NOT NULL ,
  `adminUsername`  varchar(45) NOT NULL ,
  `description`     varchar(45) NOT NULL ,
- `isPublic` binary ,
+ `isPublic` binary NOT NULL ,
 
 PRIMARY KEY (`channelName`),
 KEY `FK_26` (`adminUsername`),
