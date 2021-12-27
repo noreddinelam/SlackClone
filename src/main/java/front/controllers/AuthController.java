5270f963fd642231f38fa9e90eb617fd1cdf89d7
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2021-12-27 22:26:42 +0100

    [Controllers] add AuthController

---
+++ src/main/java/front/controllers/AuthController.java
@@ -0,0 +1,21 @@
+package front.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AuthController {
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    void onLogin(ActionEvent event) {
+
+    }
+
+}
