05b0f66e939b9462f397b3a5c365862aee7ad232
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2021-12-31 21:58:10 +0100

    [Controllers] add necessary controllers

---
+++ src/main/java/front/controllers/RequestToJoinChannelController.java
@@ -0,0 +1,22 @@
+package front.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class RequestToJoinChannelController {
+
+    @FXML
+    private ListView<String> requestsToJoinChannel;
+
+    @FXML
+    void onAccept(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRefuse(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/front/controllers/UnJoinedChannelsController.java
@@ -0,0 +1,21 @@
+package front.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class UnJoinedChannelsController {
+
+    @FXML
+    private TextField joinChannelTextFailed;
+
+    @FXML
+    private ListView<String> unJoinedChannelList;
+
+    @FXML
+    void onJoin(ActionEvent event) {
+
+    }
+
+}
